096c0bb669c2f8c647521c8abf4e40e808ff0cce
DonnuTs69 <111718022+DonnuTs69@users.noreply.github.com>
2022-11-05 12:28:06 +0700

    cart and shooping icon

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,17 @@
 import { Fragment, useContext } from "react"
 import { Outlet, Link } from "react-router-dom"
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg"
+import CartIcon from "../../components/cart-icon/cart-icon-component"
+import ChartDropdown from "../../components/cart-dropdown/cart-dropdown.component"
 import { UserContext } from "../../contexts/user.context"
 import "./navigation.styles.scss"
 
 import { signOutUser } from "../../utils/firebase/firebase.utils"
+import { CartContext } from "../../contexts/cart.context"
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext)
+  const { isCartOpen } = useContext(CartContext)
 
   // console.log(currentUser)
   return (
@@ -29,7 +33,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <ChartDropdown />}
       </div>
       <Outlet />
     </Fragment>
